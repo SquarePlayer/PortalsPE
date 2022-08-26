52253fe04d479182849103f177d9bec9189a7204
SquarePlayer <100586894+SquarePlayer@users.noreply.github.com>
2022-08-26 14:07:09 +0700

    Create BlockBreakEventListener.php

---
+++ src/Hoangg/PortalsPE/listener/BlockBreakEventListener.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+
+namespace Hoangg\PortalsPE\listener;
+
+use pocketmine\event\block\BlockBreakEvent;
+
+class BlockBreakEventListener extends BaseListener{
+
+    public function onBlockBreak(BlockBreakEvent $event) : void{
+        $ses = $this->plugin->getSessionManager()->getSession($event->getPlayer());
+        if($ses !== null){
+            if($ses->isSelectingFirstBlock()){
+                $event->setCancelled();
+                $ses->getSelection()->setFirstBlockWithFolderName($event->getBlock()->asVector3(), $event->getPlayer()->getLevel()->getFolderName());
+                $event->getPlayer()->sendMessage('First pos set');
+                $ses->setSelectingFirstBlock(false);
+            }elseif($ses->isSelectingSecondBlock()){
+                $event->setCancelled();
+                $ses->getSelection()->setSecondBlockWithFolderName($event->getBlock()->asVector3(), $event->getPlayer()->getLevel()->getFolderName());
+                $event->getPlayer()->sendMessage('Second pos set');
+                $ses->setSelectingSecondBlock(false);
+            }
+        }
+    }
+
+}
